81d74183b905a94aa800841d2574c99d34d475a1
cyh <>
2019-05-27 00:20:04 +0800

    1234init

---
+++ 1234/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  1234
+//
+//  Created by cyh on 2019/5/27.
+//  Copyright © 2019 cyh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 1234/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  1234
+//
+//  Created by cyh on 2019/5/27.
+//  Copyright © 2019 cyh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
